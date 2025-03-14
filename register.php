7914737063a6499b79163ef20a9de52e035e594e
kimaguti <kimaguti@yahoo.com>
2025-03-14 13:14:47 +0300

    its React Baibe

---
+++ register.php
@@ -0,0 +1,36 @@
+<?php
+header('Content-Type: application/json');
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user_registration";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$data = json_decode(file_get_contents("php://input"));
+
+$first_name = $data->first_name;
+$middle_name = $data->middle_name ?? '';
+$last_name = $data->last_name;
+$phone_number = $data->phone_number;
+$username = $data->username;
+$password = password_hash($data->password, PASSWORD_DEFAULT);
+$group_name = $data->group_name;
+
+$sql = "INSERT INTO users (first_name, middle_name, last_name, phone_number, username, password, group_name) 
+        VALUES ('$first_name', '$middle_name', '$last_name', '$phone_number', '$username', '$password', '$group_name')";
+
+if ($conn->query($sql) === TRUE) {
+    echo json_encode(["message" => "User registered successfully"]);
+} else {
+    echo json_encode(["message" => "Error: " . $sql . "<br>" . $conn->error]);
+}
+
+$conn->close();
+?>
